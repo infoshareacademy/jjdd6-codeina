dac99d3ae49ebce7fecf412a3de26508f5f1f826
lukasz-ratajczak <l.m.ratajczak94@gmail.com>
2019-03-29 12:23:34 +0100

    Interface 1.0. Proste menu, bez wypelnienia z jedna opcja

---
+++ src/com/company/Runner.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+
+public class Runner {
+    public void Run() throws FileNotFoundException, InterruptedException {
+
+        Inteface inteface = new Inteface();
+        int con = 1;
+        int Crypto = 0;
+        int Option = 0;
+
+        while (con != 0) {
+            if(con == 1) {Crypto = inteface.Menu();
+            if(Crypto <= 10){con = 2;}
+            else{
+                System.out.println("ERROR");
+                con = 0;}
+            }
+
+            if(con == 2){
+            Option = inteface.Options();
+            inteface.Option1(Crypto);}
+
+            int Exit = inteface.Exit();
+            if(Exit ==1){con = 0;}
+            else if(Exit == 2){con = 1;}
+            else if(Exit == 3){con = 2;}
+        }
+    }
+}
