085b9b739c273be5d907d18357385e1c06d84204
Artur Hajmowicz <a.hajmowicz@gmail.com>
2019-03-29 16:17:52 +0100

    Beginings of Calculation package

---
+++ src/Calculations/ChooseOption.java
@@ -0,0 +1,33 @@
+    package Calculations;
+
+    import java.util.Scanner;
+
+    public class ChooseOption {
+
+        public void choose(){
+
+            System.out.println("Jakie dzialanie chcialbys wykonac ?");
+            System.out.println("1. Srednia cena ");
+            System.out.println("2. Najwyzsza i najnizsza wartosc ");
+            System.out.println("3. Mediana ");
+
+            MatematicOperations matematicOperations = new MatematicOperations();
+
+            Scanner sc = new Scanner(System.in);
+            Integer option = sc.nextInt();
+
+            switch (option){
+                case 1: option.equals(1);
+                matematicOperations.average();
+
+                case 2: option.equals(2);
+                matematicOperations.extremsValue();
+
+
+
+
+
+    }
+
+
+}
