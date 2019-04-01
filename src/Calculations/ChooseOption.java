9919b72668c9349b03445f0491de91ec51f13ff9
Artur Hajmowicz <a.hajmowicz@gmail.com>
2019-04-01 16:07:23 +0200

    Package calc add

--- src/Calculations/ChooseOption.java
+++ src/Calculations/ChooseOption.java
@@ -4,7 +4,7 @@
 
     public class ChooseOption {
 
-        public void choose(){
+        public void choose() {
 
             System.out.println("Jakie dzialanie chcialbys wykonac ?");
             System.out.println("1. Srednia cena ");
@@ -16,18 +16,17 @@
             Scanner sc = new Scanner(System.in);
             Integer option = sc.nextInt();
 
-            switch (option){
-                case 1: option.equals(1);
-                matematicOperations.average();
+            switch (option) {
+                case 1:
+                    option.equals(1);
+                    matematicOperations.average();
 
-                case 2: option.equals(2);
-                matematicOperations.extremsValue();
-
-
-
-
-
-    }
+                case 2:
+                    option.equals(2);
+                    matematicOperations.extremsValue();
 
 
+            }
+
+        }
 }
