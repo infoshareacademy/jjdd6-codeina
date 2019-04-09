aee226116eb392b832cc03272b92f4dc48f9e3d0
ArturHajmowicz <33451236+ArturHajmowicz@users.noreply.github.com>
2019-04-09 16:07:32 +0200

    MathematicCalculation (#16)
    * Beginings of Calculation package
    
    * Package calc add
    
    * Calculation interface updated
    
    * update
    
    * Try
    
    * try to solve issue with methods
    
    * tones of issues
    
    * Calculation Done!
    
    * Calculation Done and check with example map
    
    * Commit with example map - Next commit will be ready to marge
    
    * Calculation ready to add
    
    * Ready
    
    * Remarks Checked! MathematicOperation with mediana, average and highest

---
+++ src/com/calculations/MathematicOperation.java
@@ -0,0 +1,51 @@
+package com.calculations;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MathematicOperation {
+
+    public Double average(Map<LocalDate, Double> fromCsv) {
+                Double sumOfElements = fromCsv.values().stream()
+                .collect(Collectors.summingDouble(Double::doubleValue));
+        System.out.println(sumOfElements / fromCsv.size());
+
+        return sumOfElements / fromCsv.size();
+    }
+
+    public Map.Entry<LocalDate, Double> findExtremeValue(Map<LocalDate, Double> fromCsv) {
+
+        Map.Entry<LocalDate, Double> highestValue = fromCsv.entrySet().stream()
+                .max(Map.Entry.comparingByValue(Comparator.comparingDouble(Double::doubleValue)))
+                .get();
+
+        System.out.println(highestValue);
+        return highestValue;
+    }
+
+    public double median(Map<LocalDate, Double> fromCsv) {
+
+        List<Double> arrayOfValues = new ArrayList<>(fromCsv.values());
+
+        int lengthOfArray = arrayOfValues.size();
+        int med = lengthOfArray / 2;
+
+        arrayOfValues = arrayOfValues.stream()
+                .sorted((o1, o2) -> (int) (o1 - o2))
+                .collect(Collectors.toList());
+
+        if (lengthOfArray % 2 == 0) {
+            med = (lengthOfArray + 1) / 2;
+            System.out.println("50% of records are equal or smaller than " + arrayOfValues.get(med) + ".");
+            System.out.println("50% of records are equal or bigger than " + arrayOfValues.get(med) + ".");
+            return med;
+        }
+
+        System.out.println("50% of records are equal or smaller than " + arrayOfValues.get(med) + ".");
+        System.out.println("50% of records are equal or bigger than " + arrayOfValues.get(med) + ".");
+        return med;
+    }
+}
+
+
