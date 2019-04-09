adc76d069378ccfcfb3e11164d3f51435cbacf02
Artur Hajmowicz <a.hajmowicz@gmail.com>
2019-04-09 14:24:11 +0200

    Commit with example map - Next commit will be ready to marge

--- src/com/calculations/ChooseOption.java
+++ src/com/calculations/ChooseOption.java
@@ -14,37 +14,38 @@ public class ChooseOption {
         LoadingData loadingData = new LoadingData();
 
         //TODO
-// Tu bede przyjmowac mape! Janek nad nia pracuje 05.04.2019
-//        List<CryptoCurrency> ld = loadingData.getCryptoCurrencyFromDateToDate("csv/btc.csv");
-
+//        Tu bedzie przyjmowana mapa. Ponizej przykladowa
         ExampleMapToBeRemoved exampleMapToBeRemoved = new ExampleMapToBeRemoved();
 
         System.out.println("What You want to see ?");
         System.out.println("1. Average price ");
         System.out.println("2. Highest / Lowest price ");
         System.out.println("3. Mediana ");
+        System.out.println("4. Go back to main manu ");
 
         MatematicOperations matematicOperations = new MatematicOperations();
 
         Scanner sc = new Scanner(System.in);
         Integer option = sc.nextInt();
 
-
         switch (option) {
             case 1:
                 option.equals(1);
                 matematicOperations.average(exampleMapToBeRemoved.exampleMap());
-                break;
+                choose();
 
             case 2:
                 option.equals(2);
                 matematicOperations.findExtremeValue(exampleMapToBeRemoved.exampleMap());
-                break;
+                choose();
 
             case 3:
                 option.equals(3);
                 matematicOperations.median(exampleMapToBeRemoved.exampleMap());
-                break;
+                choose();
+
+            case 4:
+
 
             default:
                 System.out.println("Choose again, wrong number!");
--- src/com/calculations/MatematicOperations.java
+++ src/com/calculations/MatematicOperations.java
@@ -1,17 +1,18 @@
 package com.calculations;
 
-import javax.xml.crypto.Data;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class MatematicOperations {
 
+
     public Double average(Map<LocalDate, Double> fromCsv) {
 
         Double sumOfElements = fromCsv.values().stream()
                 .collect(Collectors.summingDouble(Double::doubleValue));
-        System.out.println(sumOfElements/fromCsv.size());
+        System.out.println(sumOfElements / fromCsv.size());
+
         return sumOfElements / fromCsv.size();
     }
 
@@ -20,6 +21,7 @@ public class MatematicOperations {
         Map.Entry<LocalDate, Double> highestValue = fromCsv.entrySet().stream()
                 .max(Map.Entry.comparingByValue(Comparator.comparingDouble(Double::doubleValue)))
                 .get();
+
         System.out.println(highestValue);
         return highestValue;
     }
@@ -28,23 +30,22 @@ public class MatematicOperations {
 
         List<Double> arrayOfValues = new ArrayList<>(fromCsv.values());
 
-        int lengthOfArray =  arrayOfValues.size();
-        int med = lengthOfArray/2;
+        int lengthOfArray = arrayOfValues.size();
+        int med = lengthOfArray / 2;
 
         arrayOfValues = arrayOfValues.stream()
                 .sorted((o1, o2) -> (int) (o1 - o2))
                 .collect(Collectors.toList());
 
-
         if (lengthOfArray % 2 == 0) {
-            med = (lengthOfArray + 1) /2;
+            med = (lengthOfArray + 1) / 2;
             System.out.println("50% of records are equal or smaller than " + arrayOfValues.get(med) + ".");
             System.out.println("50% of records are equal or bigger than " + arrayOfValues.get(med) + ".");
             return med;
         }
 
-            System.out.println("50% of records are equal or smaller than " + arrayOfValues.get(med) + ".");
-            System.out.println("50% of records are equal or bigger than " + arrayOfValues.get(med) + ".");
+        System.out.println("50% of records are equal or smaller than " + arrayOfValues.get(med) + ".");
+        System.out.println("50% of records are equal or bigger than " + arrayOfValues.get(med) + ".");
         return med;
     }
 }
