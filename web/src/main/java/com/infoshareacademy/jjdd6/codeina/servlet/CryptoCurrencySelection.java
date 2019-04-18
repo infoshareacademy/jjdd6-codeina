c99e762432f8a2a8ab55b76fce28b2122c7cc461
Jan Ostrowski <jan.ostrowski1337@gmail.com>
2019-04-18 16:24:49 +0200

    proba tworzenia servleta

--- web/src/main/java/com/infoshareacademy/jjdd6/codeina/servlet/CryptoCurrencySelection.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/codeina/servlet/CryptoCurrencySelection.java
@@ -1,7 +1,10 @@
 package com.infoshareacademy.jjdd6.codeina.servlet;
 
+import com.infoshareacademy.jjdd6.CryptoCurrency;
 import com.infoshareacademy.jjdd6.TemplateProvider;
 import com.infoshareacademy.jjdd6.codeina.service.CryptoService;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -10,6 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet("/selection")
 public class CryptoCurrencySelection extends HttpServlet {
@@ -18,8 +23,22 @@ public class CryptoCurrencySelection extends HttpServlet {
 
     @Inject
     CryptoService cryptoService ;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String choice =req.getParameter("choice") ;
+
+        Template template = templateProvider.getTemplate(getServletContext(),"test.ftlh");
+        Map<String, CryptoCurrency> model = new HashMap<>();
+
+        CryptoCurrency cryptoCurrency=  cryptoService.getNewestDate(choice);
+        model.put("choice",cryptoCurrency);
+
+        try {
+            template.process(model,resp.getWriter());
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
 
     }
 }
