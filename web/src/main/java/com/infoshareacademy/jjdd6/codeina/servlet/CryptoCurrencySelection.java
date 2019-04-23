582494415a5d5a405f532993dddd848b0c234106
Lukasz Ratajczak <miszeq12@gmail.com>
2019-04-23 19:35:42 +0200

    added whole index.ftlh and html

--- web/src/main/java/com/infoshareacademy/jjdd6/codeina/servlet/CryptoCurrencySelection.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/codeina/servlet/CryptoCurrencySelection.java
@@ -32,7 +32,7 @@ public class CryptoCurrencySelection extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String choice =req.getParameter("choice") ;
 
-        Template template = templateProvider.getTemplate(getServletContext(),"fm-templates\\index.ftlh");
+        Template template = templateProvider.getTemplate(getServletContext(),"D:\\Coder\\jjdd6-codeina\\web\\src\\main\\webapp\\WEB-INF\\fm-templates\\test.ftlh");
         Map<String, String> model = new HashMap<>();
 
         CryptoCurrency cryptoCurrency=  cryptoService.getNewestDate(choice);
