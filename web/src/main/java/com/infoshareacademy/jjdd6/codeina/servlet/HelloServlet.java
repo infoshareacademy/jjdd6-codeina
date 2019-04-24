6e5ad2190e4d07e813752b6afb4955b9dfe8d335
Lukasz Ratajczak <l.m.ratajczak94@gmail.com>
2019-04-24 16:53:54 +0200

    reformat ftlh

--- web/src/main/java/com/infoshareacademy/jjdd6/codeina/servlet/HelloServlet.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/codeina/servlet/HelloServlet.java
@@ -1,23 +1,56 @@
 package com.infoshareacademy.jjdd6.codeina.servlet;
 
 import com.infoshareacademy.jjdd6.CryptoCurrency;
+import com.infoshareacademy.jjdd6.TemplateProvider;
+import com.infoshareacademy.jjdd6.codeina.service.CryptoService;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
+    @Inject
+    TemplateProvider templateProvider ;
+
+    @Inject
+    CryptoService cryptoService ;
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello from currency app");
 
-        CryptoCurrency cryptoCurrency = new CryptoCurrency();
 
-        resp.getWriter().println("And with empty crypto: " + cryptoCurrency);
+
+//        String choice =req.getParameter("choice") ;
+
+
+        Template template = templateProvider.getTemplate(getServletContext(),"index.ftlh");
+        Map<String, String> model = new HashMap<>();
+
+//        CryptoCurrency cryptoCurrency=  cryptoService.getNewestDate(choice);
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
+
+//        LocalDate localDate = cryptoCurrency.getDate() ;
+//        model.put("lastPrice",String.valueOf(cryptoCurrency.getPrice()));
+
+
+
+        try {
+            template.process(model,resp.getWriter());
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+
     }
 }
