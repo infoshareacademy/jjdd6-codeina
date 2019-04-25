941ccb26123bdef3dafa206d7e02ab51494aac6f
Łukasz Ratajczak <43352123+lukasz-ratajczak@users.noreply.github.com>
2019-04-25 14:29:35 +0200

    Frontend (#35)
    * added war plus config/template providers
    
    * its working now !
    
    * servlet_numero_uno
    
    * initial
    
    * proba tworzenia servleta
    
    * added index.html
    
    * upgrade
    
    * first servlet
    
    * upgrade html
    
    * reformat html
    
    * added whole index.ftlh and html
    
    * reformat ftlh
    
    * first try to show sth

--- web/src/main/java/com/infoshareacademy/jjdd6/codeina/servlet/HelloServlet.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/codeina/servlet/HelloServlet.java
@@ -1,23 +1,37 @@
 package com.infoshareacademy.jjdd6.codeina.servlet;
 
-import com.infoshareacademy.jjdd6.CryptoCurrency;
+import com.infoshareacademy.jjdd6.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
+    @Inject
+    TemplateProvider templateProvider;
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello from currency app");
 
-        CryptoCurrency cryptoCurrency = new CryptoCurrency();
+        Template template = templateProvider.getTemplate(getServletContext(), "index.ftlh");
+        Map<String, String> model = new HashMap<>();
 
-        resp.getWriter().println("And with empty crypto: " + cryptoCurrency);
+
+        try {
+            template.process(model, resp.getWriter());
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+
     }
 }
