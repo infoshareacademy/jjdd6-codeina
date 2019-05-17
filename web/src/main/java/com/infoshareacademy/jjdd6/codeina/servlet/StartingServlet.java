46158e6ca6c9952484e82641c586b0a19414ffd1
Jan Ostrowski <jan.ostrowski1337@gmail.com>
2019-05-17 21:06:08 +0200

    servlet do stronki startowej

---
+++ web/src/main/java/com/infoshareacademy/jjdd6/codeina/servlet/StartingServlet.java
@@ -0,0 +1,35 @@
+package com.infoshareacademy.jjdd6.codeina.servlet;
+
+import com.infoshareacademy.jjdd6.codeina.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+@WebServlet("/")
+public class StartingServlet extends HttpServlet {
+    private static final Logger logger = Logger.getLogger(StartingServlet.class.getName());
+    @Inject
+    private TemplateProvider templateProvider
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Template template = templateProvider.getTemplate(getServletContext(),"start.ftlh");
+        Map<String,Object> model = new HashMap<>();
+        model.put("1",1);
+        try {
+            template.process(model,resp.getWriter());
+        } catch (TemplateException e) {
+            logger.severe(e.getMessage());
+        }
+    }
+}
